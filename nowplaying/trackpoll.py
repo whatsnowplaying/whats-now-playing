76271047e873d3c78b64ac06ca958aba2e3034c7
Allen Wittenauer <aw@apache.org>
2021-09-18 21:39:29 -0700

    Add a traceback since it could be anywhere

--- nowplaying/trackpoll.py
+++ nowplaying/trackpoll.py
@@ -3,6 +3,7 @@
 
 import logging
 import threading
+import traceback
 
 from PySide2.QtCore import Signal, QThread  # pylint: disable=no-name-in-module
 
@@ -60,6 +61,7 @@ class TrackPoll(QThread):  # pylint: disable=too-many-instance-attributes
                 self.gettrack()
             except Exception as error:  #pylint: disable=broad-except
                 logging.debug('Failed attempting to get a track: %s', error)
+                logging.debug(traceback.print_stack())
 
     def __del__(self):
         logging.debug('TrackPoll is being killed!')
