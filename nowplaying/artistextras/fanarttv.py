57b881a23e8857ba4abfee2f5094b7ed3590c7dc
Allen Wittenauer <aw@apache.org>
2022-11-13 10:17:24 -0800

    fix pylint

--- nowplaying/artistextras/fanarttv.py
+++ nowplaying/artistextras/fanarttv.py
@@ -24,6 +24,22 @@ class Plugin(ArtistExtrasPlugin):
         self.version = nowplaying.version.get_versions()['version']
         super().__init__(config=config, qsettings=qsettings)
 
+    @staticmethod
+    def _fetch(apikey, artistid):
+        artistrequest = None
+        try:
+            baseurl = f'http://webservice.fanart.tv/v3/music/{artistid}'
+            logging.debug('fanarttv: calling %s', baseurl)
+            artistrequest = requests.get(f'{baseurl}?api_key={apikey}',
+                                         timeout=5)
+        except (requests.exceptions.ReadTimeout,
+                urllib3.exceptions.ReadTimeoutError, socket.timeout):
+            logging.error('fantart.tv timeout getting artistid %s', artistid)
+        except Exception as error:  # pylint: disable=broad-except
+            logging.error('fanart.tv: %s', error)
+
+        return artistrequest
+
     def download(self, metadata=None, imagecache=None):  # pylint: disable=too-many-branches
         ''' download the extra data '''
 
@@ -47,19 +63,10 @@ class Plugin(ArtistExtrasPlugin):
         logging.debug('got musicbrainzartistid: %s',
                       metadata['musicbrainzartistid'])
         for artistid in metadata['musicbrainzartistid']:
-            try:
-                baseurl = f'http://webservice.fanart.tv/v3/music/{artistid}'
-                logging.debug('fanarttv: calling %s', baseurl)
-                artistrequest = requests.get(f'{baseurl}?api_key={apikey}',
-                                             timeout=5)
-            except (requests.exceptions.ReadTimeout,
-                    urllib3.exceptions.ReadTimeoutError, socket.timeout):
-                logging.error('fantart.tv timeout getting artistid %s',
-                              artistid)
+            artistrequest = self._fetch(apikey, artistid)
+            if not artistrequest:
                 return None
-            except Exception as error:  # pylint: disable=broad-except
-                logging.error('fanart.tv: %s', error)
-                return None
+
             artist = artistrequest.json()
 
             if artist.get('name') and nowplaying.utils.normalize(
