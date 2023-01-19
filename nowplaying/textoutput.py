820aaafae505bca651b1763c86ee8c8f687cde4a
Allen Wittenauer <aw@apache.org>
2023-01-19 07:38:37 -0800

    sourcery

--- nowplaying/textoutput.py
+++ nowplaying/textoutput.py
@@ -31,9 +31,9 @@ def writetxttrack(config=None,
 
     if not filename:
         filename = config.cparser.value('textoutput/file')
-        if not filename:
-            logging.debug('no filename passwed or configured?!?')
-            return
+    if not filename:
+        logging.debug('no filename passwed or configured?!?')
+        return
 
     if config and config.cparser.value('textoutput/fileappend', type=bool):
         mode = 'a'
