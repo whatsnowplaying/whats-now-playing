67f648eb5fa120658e4384942381e5ede5bdbb90
Allen Wittenauer <aw@apache.org>
2023-03-15 20:25:24 -0700

    try2

--- nowplaying/inputs/json.py
+++ nowplaying/inputs/json.py
@@ -74,7 +74,7 @@ class Plugin(InputPlugin):
             return {}
 
         try:
-            with open(filepath, mode='rb', encoding='utf-8') as fhin:
+            with open(filepath, mode='rb') as fhin:
                 return json.load(fhin)
         except Exception as error:  # pylint: disable=broad-except
             logging.error(error)
