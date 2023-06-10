e6157080e6756895e4317b0c7ab8593a4877c74c
Allen Wittenauer <aw@apache.org>
2023-06-10 05:47:08 -0700

    Add some unit tests to twitch chat perms

---
+++ tests/test_twitchchat.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+''' test winmedia ... ok, not really. '''
+
+import asyncio
+
+import pytest
+import nowplaying.twitch.chat  # pylint: disable=import-error
+
+
+def setup_generic_commands(config):
+    ''' setup a bunch of default commands to test against '''
+    for command in nowplaying.twitch.chat.TWITCHBOT_CHECKBOXES:
+        cmd = f'twitchbot-command-{command}'
+        for checkbox in nowplaying.twitch.chat.TWITCHBOT_CHECKBOXES:
+            if command == checkbox:
+                config.cparser.setValue(f'{cmd}cmd/{checkbox}', True)
+            else:
+                config.cparser.setValue(f'{cmd}cmd/{checkbox}', False)
+
+@pytest.mark.asyncio
+async def test_twitchchat_perms(bootstrap):  # pylint: disable=redefined-outer-name
+    ''' test twitchbot perms '''
+    config = bootstrap
+    stopevent = asyncio.Event()
+
+    setup_generic_commands(config)
+    config.cparser.sync()
+    chat = nowplaying.twitch.chat.TwitchChat(config=config, stopevent=stopevent)
+
+    streamerprofile = {'broadcaster': '1', 'subscriber': '9'}
+    moderatorprofile = {'moderator': '1', 'subscriber': '24'}
+    hypetrainprofile = {'vip':'1','subscriber':'3012','hype-train':'1'}
+
+    assert chat.check_command_perms(streamerprofile, 'broadcastercmd')
+    assert chat.check_command_perms(streamerprofile, 'subscribercmd')
+    assert chat.check_command_perms(streamerprofile, 'anyonecmd')
+    assert not chat.check_command_perms(streamerprofile, 'moderatorcmd')
+    assert chat.check_command_perms(moderatorprofile, 'anyonecmd')
+    assert chat.check_command_perms(moderatorprofile, 'moderatorcmd')
+    assert not chat.check_command_perms(moderatorprofile, 'broadcastercmd')
+
+    stopevent.set()
